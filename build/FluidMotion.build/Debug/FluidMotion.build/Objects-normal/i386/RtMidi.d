d78038c405214c7d0904784da05de1157e66c0ec
Byron <byron@g-04-412-imac.mddn.vuw.ac.nz>
2012-11-05 23:47:01 +1300

    Updated .gitignore to exclude build folders and compiled .app files

--- build/FluidMotion.build/Debug/FluidMotion.build/Objects-normal/i386/RtMidi.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/mystfit/Code/of_0071_osx_release/FluidMotion/apps/myApps/example-fluid/../../../addons/ofxMidi/src/desktop/rtmidi/RtMidi.cpp \
-  /Users/mystfit/Code/of_0071_osx_release/FluidMotion/apps/myApps/example-fluid/../../../addons/ofxMidi/src/ofxMidiConstants.h \
-  ../../../libs/openFrameworks/utils/ofConstants.h \
-  ../../../libs/glew/include/GL/glew.h \
-  ../../../libs/tess2/include/tesselator.h \
-  /Users/mystfit/Code/of_0071_osx_release/FluidMotion/apps/myApps/example-fluid/../../../addons/ofxMidi/src/desktop/rtmidi/RtMidi.h \
-  /Users/mystfit/Code/of_0071_osx_release/FluidMotion/apps/myApps/example-fluid/../../../addons/ofxMidi/src/desktop/rtmidi/RtError.h
